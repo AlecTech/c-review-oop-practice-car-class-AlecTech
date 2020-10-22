4008fd13d9f72d30f239616cbadfabce0f968d3b
AlecTech <eremeevoleg@gmail.com>
2020-10-22 13:26:36 -0600

    Initial commit

---
+++ oct22CarClassPracticeOleg/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace oct22CarClassPracticeOleg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Chevy", "Camaro", "Red", "MaxOccupancy", "FuelEffeciency", new Odometer(101), new FuelTank(50));
+
+            Console.WriteLine(myCar.gasTank);
+
+            myCar.Drive();
+
+
+        }
+    }
+}
