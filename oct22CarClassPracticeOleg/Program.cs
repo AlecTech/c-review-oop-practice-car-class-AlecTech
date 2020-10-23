ec12ed0b401e1d05f650ea52da4a326bca629209
AlecTech <eremeevoleg@gmail.com>
2020-10-22 23:26:27 -0600

    Added Tests for threshhold odometer and default constructor when FuelTank has no parameters and test when not enough fuel thors exception is commented out;

--- oct22CarClassPracticeOleg/Program.cs
+++ oct22CarClassPracticeOleg/Program.cs
@@ -10,7 +10,41 @@ namespace oct22CarClassPracticeOleg
 
             Console.WriteLine(myCar.ToString());
 
+            myCar.Drive(30);
 
+            Console.WriteLine(myCar.ToString());
+
+            myCar.Drive(100);
+
+            Console.WriteLine(myCar.ToString());
+
+            myCar.Drive(500);
+
+            Console.WriteLine(myCar.ToString());
+
+            //in next 20 km we will runout of fuel(throw and exception)
+            //myCar.Drive(20);
+
+            //Console.WriteLine(myCar.ToString());
+
+            //this Car2 will runover 1000000 and reset its counter to 0
+            Car myCar2 = new Car("Ford", "Focus", "Red", 5, 7.8, new Odometer(999980), new FuelTank(50, 40));
+
+            Console.WriteLine(myCar2.ToString());
+
+            myCar2.Drive(30);
+
+            Console.WriteLine(myCar2.ToString());
+
+            myCar2.Drive(100);
+
+            Console.WriteLine(myCar2.ToString());
+            //this car3 will test if default constructor works for the fuelTank
+            Car myCar3 = new Car("Toyota", "Highlander", "White", 7, 12.3, new Odometer(200000), new FuelTank());
+
+            Console.WriteLine(myCar3.ToString());
+
+            myCar3.Drive(30);
 
             Console.ReadLine();
             
